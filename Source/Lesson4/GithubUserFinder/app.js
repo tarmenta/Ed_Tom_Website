0d1e38b1e61e48a3f57a858163ddb637ca76c13c
tarmenta <51331495+tarmenta@users.noreply.github.com>
2020-06-18 12:40:18 -0500

    update

--- Source/Lesson4/GithubUserFinder/app.js
+++ Source/Lesson4/GithubUserFinder/app.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
     $('#searchGithub').on('keyup', function(s) {
         let searchUser = s.target.value;
 
-        //GitHub request for searches
+        /*GitHub request for searches*/
         $.ajax({
             url:'https://api.github.com/users/'+ searchUser,
             data:{
@@ -12,18 +12,21 @@ $(document).ready(function(){
             }
         }).done(function(user) {
 
+            /*Sorting out Name and Displaying it*/
             $('#userName').html(`
             <div>
               <h3>${user.name}</h3>
               </div>
             `);
 
+            /*Sorting out user ID and Displaying it*/
             $('#userId').html(`
             <div>
               <h3>Id:${user.id}</h3>
               </div>
               `);
 
+            /*Sorting out profile picture and Displaying it*/
             $('#userPicture').html(`
               <div>
               <p>profile picture</p>
@@ -31,13 +34,12 @@ $(document).ready(function(){
               </div>
               `);
 
+            /*Sorting out the link to github user and Displaying it*/
             $('#userLink').html(`
             <div>
             <a class="button" href="${user.html_url}" target="_blank">Link to ${user.name}</a>
               </div>
               `);
-             
-
         });
     });
 });
