989495696c2bbeb9a3ca8a78bab73c19f8a77977
tarmenta <51331495+tarmenta@users.noreply.github.com>
2020-06-25 05:43:08 -0500

    Todo list done

---
+++ Source/Lesson5/TodoList/todos/todos.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import {Todo} from '../../models/Todo';
+ import {TodoService} from '../../services/todo.service';
+
+
+@Component({
+  selector: 'app-todos',
+  templateUrl: './todos.component.html',
+  styleUrls: ['./todos.component.css']
+})
+export class TodosComponent implements OnInit {
+  todos: Todo[];
+
+   constructor(private todoService: TodoService) { }
+
+  ngOnInit(): void {
+    this.todoService.getTodos().subscribe(todos =>{
+      this.todos = todos;
+    });
+  }
+
+
+  deleteTodo(todo: Todo){
+    // Delete UI
+    this.todos = this.todos.filter(t => t.id !== todo.id);
+    // Delete from server
+    this.todoService.deleteTodo(todo).subscribe();
+  }
+// Add to the list
+  addTodo(todo: Todo){
+    this.todoService.addTodo(todo).subscribe(todo => {
+      this.todos.push(todo);
+    });
+  }
+}
